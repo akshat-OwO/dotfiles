2bff2f3594702a8d69672e945e2dfd0e8b859032
akshat <akshatsinghdelhi@gmail.com>
2025-03-14 18:39:00 +0530

    fix: formatter

--- nvim/.config/nvim/lua/akshat/plugins/format.lua
+++ nvim/.config/nvim/lua/akshat/plugins/format.lua
@@ -1,64 +1,69 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            local conform = require("conform")
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local conform = require("conform")
 
-            conform.setup({
-                formatters_by_ft = {
-                    javascript = { "prettier" },
-                    typescript = { "prettier" },
-                    javascriptreact = { "prettier" },
-                    typescriptreact = { "prettier" },
-                    css = { "prettier" },
-                    html = { "prettier" },
-                    json = { "prettier" },
-                    yaml = { "prettier" },
-                    markdown = { "prettier" },
-                    graphql = { "prettier" },
-                    lua = { "stylua" },
-                    python = { "isort", "black" },
-                    go = { "gofmt" },
-                },
-                formatters = {
-                    prettier = {
-                        command = "prettier",
-                        args = {
-                            "--stdin-filepath",
-                            "$FILENAME",
-                            "--tab-width",
-                            "4",
-                            "--print-width",
-                            "120",
-                            "--use-tabs",
-                            "false",
-                            "--trailing-comma",
-                            "all",
-                            "--semi",
-                            "true",
-                        },
-                        stdin = true,
-                        cwd = function()
-                            return vim.fs.dirname(vim.fs.find({ ".prettierrc", ".prettierrc.json", ".prettierrc.js" }, { upward = true })[1])
-                        end,
-                    },
-                },
-                format_on_save = {
-                    lsp_fallback = true,
-                    async = false,
-                    timeout_ms = 500,
-                },
-            })
+			conform.setup({
+				formatters_by_ft = {
+					javascript = { "prettier" },
+					typescript = { "prettier" },
+					javascriptreact = { "prettier" },
+					typescriptreact = { "prettier" },
+					css = { "prettier" },
+					html = { "prettier" },
+					json = { "prettier" },
+					yaml = { "prettier" },
+					markdown = { "prettier" },
+					graphql = { "prettier" },
+					lua = { "stylua" },
+					python = { "isort", "black" },
+					go = { "gofmt" },
+				},
+				formatters = {
+					prettier = {
+						command = "prettier",
+						args = function()
+							local config_file = vim.fs.find({ ".prettierrc", ".prettierrc.json", ".prettierrc.js" }, { upward = true })[1]
+							if config_file then
+								return { "--stdin-filepath", "$FILENAME" }
+							end
+							return {
+								"--stdin-filepath",
+								"$FILENAME",
+								"--tab-width",
+								"4",
+								"--print-width",
+								"120",
+								"--use-tabs",
+								"true",
+								"--trailing-comma",
+								"all",
+								"--semi",
+								"true",
+							}
+						end,
+						stdin = true,
+						cwd = function()
+							local config_file = vim.fs.find({ ".prettierrc", ".prettierrc.json", ".prettierrc.js" }, { upward = true })[1]
+							if config_file then
+								return vim.fs.dirname(config_file)
+							end
+							return vim.fs.dirname(vim.api.nvim_buf_get_name(0))
+						end,
+					},
+				},
+			})
 
-            -- Keymapping for manual formatting
-            vim.keymap.set({ "n", "v" }, "<leader>f", function()
-                conform.format({
-                    lsp_fallback = true,
-                    async = false,
-                    timeout_ms = 500,
-                })
-            end, { desc = "[F]ormat file or range (in visual mode)", silent = true })
-        end,
-    },
+			-- Keymapping for manual formatting
+			vim.keymap.set({ "n", "v" }, "<leader>f", function()
+				conform.format({
+					lsp_fallback = true,
+					async = true,
+					timeout_ms = 2000,
+				})
+			end, { desc = "[F]ormat file or range (in visual mode)", silent = true })
+		end,
+	},
 }
